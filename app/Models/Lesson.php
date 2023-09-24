1eca1835920bf3772e02cd19d34817d0a9cdf55b
Marco Peter <marco@peter-net.ch>
2023-09-25 00:39:06 +0200

    Add lessons

--- app/Models/Lesson.php
+++ app/Models/Lesson.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -10,6 +11,16 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 class Lesson extends Model
 {
     use HasFactory;
+
+    public function getStartAttribute($value)
+    {
+        return (new Carbon($value))->format('Y-m-d H:i');
+    }
+
+    public function getFinishAttribute($value)
+    {
+        return (new Carbon($value))->format('Y-m-d H:i');
+    }
 
     public function users(): BelongsToMany
     {
--- resources/views/management/edit-course.blade.php
+++ resources/views/management/edit-course.blade.php
@@ -5,79 +5,129 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div
-                class="text-gray-800 dark:text-gray-200 bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-                <form method="POST" action="{{ route('courses.update', $course) }}">
-                    @csrf
-                    @method('patch')
-                    <label for="name">Name</label>
-                    <p>
-                        <input class="text-gray-800" type="text" name="name" value="{{ $course->name }}">
-                    <p>
-                        <label for="description">Description</label>
-                    <p>
-                        <textarea class="text-gray-800" name="description" id="description" cols="30" rows="10">{{ $course->description }}</textarea>
-                    <p>
-                        <label for="fee">Fee</label>
-                    <p>
-                        <input class="text-gray-800" type="text" name="fee" id="fee"
-                            value="{{ $course->fee }}">
-                    <p>
-                        <label for="capacity">Capacity</label>
-                    <p>
-                        <input class="text-gray-800" type="text" name="capacity" id="capacity"
-                            value="{{ $course->capacity }}">
-                    <p>
-                        <button name="changed_item" value="field" type="submit">Change</button>
-                    <p>
-                        <label for="teams">Teams</label>
-                    <p>
-                    <table>
+    <form method="POST" action="{{ route('courses.update', $course) }}">
+        @csrf
+        @method('patch')
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div
+                    class="text-gray-800 dark:text-gray-200 bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
+                    <div class="col-span-6 sm:col-span-4">
+                        <x-label for="name" value="Name" />
+                        <x-input id="name" name="name" type="text" class="mt-1 block w-full" required
+                            autocomplete="name" value="{{ $course->name }}" />
+                        <x-input-error for="name" class="mt-2" />
+                    </div>
+                    <div class="col-span-6 sm:col-span-4">
+                        <x-label for="description" value="Description" />
+                        <x-input id="description" name="description" type="text" class="mt-1 block w-full"
+                            value="{{ $course->description }}" />
+                        <x-input-error for="description" class="mt-2" />
+                    </div>
+                    <div class="col-span-6 sm:col-span-4">
+                        <x-label for="fee" value="Fee" />
+                        <x-input id="fee" name="fee" type="integer" class="mt-1 block w-full"
+                            value="{{ $course->fee }}" />
+                        <x-input-error for="fee" class="mt-2" />
+                    </div>
+                    <div class="col-span-6 sm:col-span-4">
+                        <x-label for="capacity" value="Capacity" />
+                        <x-input id="capacity" name="capacity" type="integer" class="mt-1 block w-full"
+                            value="{{ $course->capacity }}" />
+                        <x-input-error for="capacity" class="mt-2" />
+                    </div>
+                    <button name="changed_item" value="field" type="submit"
+                        class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">Change</button>
+                </div>
+            </div>
+        </div>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div
+                    class="text-gray-800 dark:text-gray-200 bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
+                    <h1>Teams</h1>
+                    <table class="w-full">
                         <caption>Automatically signed in</caption>
                         <tbody>
                             @foreach ($course->teams_signed_in as $team)
                                 <tr>
-                                    <td><button name="remove_team" value="{{ $team->id }}"
-                                            type="submit">Remove</button></td>
-                                    <td><button name="autosign_off" value="{{ $team->id }}", type="submit">Manual
+                                    <td><button name="remove_team" value="{{ $team->id }}" type="submit"
+                                            class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">Remove</button>
+                                    </td>
+                                    <td><button name="autosign_off" value="{{ $team->id }}", type="submit"
+                                            class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">Manual
                                             Sign-In</button></td>
                                     <td>{{ $team->name }}</td>
                                 </tr>
                             @endforeach
                         </tbody>
                     </table>
-
-                    <table>
+                </div>
+            </div>
+        </div>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div
+                    class="text-gray-800 dark:text-gray-200 bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
+                    <table class="w-full">
                         <caption>Sign in manually</caption>
                         <tbody>
                             @foreach ($course->teams_not_signed_in as $team)
                                 <tr>
-                                    <td><button name="remove_team" value="{{ $team->id }}"
-                                            type="submit">Remove</button></td>
-                                    <td><button name="autosign_on" value="{{ $team->id }}" type="submit">Auto
+                                    <td><button name="remove_team" value="{{ $team->id }}" type="submit"
+                                            class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">Remove</button>
+                                    </td>
+                                    <td><button name="autosign_on" value="{{ $team->id }}" type="submit"
+                                            class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">Auto
                                             Sign-In</button></td>
                                     <td>{{ $team->name }}</td>
                                 </tr>
                             @endforeach
                         </tbody>
                     </table>
-
-                    <table>
+                </div>
+            </div>
+        </div>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div
+                    class="text-gray-800 dark:text-gray-200 bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
+                    <table class="w-full">
                         <caption>Available teams</caption>
                         <tbody>
                             @foreach ($teams_not_selected as $team)
                                 <tr>
-                                    <td><button name="add_team" value="{{ $team->id }}" type="submit">Add</button>
+                                    <td><button name="add_team" value="{{ $team->id }}" type="submit"
+                                            class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">Add</button>
                                     </td>
                                     <td>{{ $team->name }}</td>
                                 </tr>
                             @endforeach
                         </tbody>
                     </table>
-                </form>
+                </div>
             </div>
         </div>
-    </div>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div
+                    class="text-gray-800 dark:text-gray-200 bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
+                    <table>
+                        <thead>
+                            <th>Start</th>
+                            <th>End</th>
+                        </thead>
+                        <tbody>
+                            @foreach ($course->lessons as $lesson)
+                            <tr>
+                                <td>{{ $lesson->start }}</td>
+                                <td>{{ $lesson->finish }}</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </form>
 </x-app-layout>
