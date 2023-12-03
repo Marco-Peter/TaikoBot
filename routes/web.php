c6f2fb6825c47386fdcb4be8993b52d23a82c925
Marco Peter <marco@peter-net.ch>
2023-12-03 22:27:48 +0100

    Add Course controller to routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CourseController;
 use App\Http\Controllers\UserController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -35,4 +36,5 @@ Route::middleware([
     })->name('dashboard');
 
     Route::resource('users', UserController::class);
+    Route::resource('courses', CourseController::class);
 });
