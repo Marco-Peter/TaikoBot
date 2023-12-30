2bd4c71caeaca0083bc5d7704ebd718085dc3f73
Marco Peter <marco@peter-net.ch>
2023-12-30 11:08:05 +0100

    Start with language support for German

---
+++ lang/de/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Dein Passwort wurde zurückgesetzt.',
+    'sent' => 'Der Link um das Passwort zurückzusetzen wurde verschickt.',
+    'throttled' => 'Warte bitte vor dem nächsten Versuch.',
+    'token' => 'Dieses Token zum zurücksetzen des Passwortes ist inkorrekt.',
+    'user' => "Es existiert kein Benutzer mit dieser Email-Adresse.",
+
+];
