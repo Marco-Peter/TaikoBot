f0e860424b845dfe7dc92bb1c8e7e9e41edf9d16
Marco Peter <marco@peter-net.ch>
2024-01-04 15:32:56 +0100

    Extend data structure to hold threads

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Message extends Model
 {
@@ -15,13 +16,35 @@ class Message extends Model
         'user_id',
     ];
 
+    /**
+     * To which channel is this message posted?
+     */
     public function messageChannel(): BelongsTo
     {
         return $this->belongsTo(MessageChannel::class);
     }
 
+    /**
+     * Which poster die write this message?
+     */
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get all replies (if any) to a message
+     */
+    public function replies(): HasMany
+    {
+        return $this->hasMany(Message::class);
+    }
+
+    /**
+     * Get the original message, if this is a reply
+     */
+    public function original(): BelongsTo
+    {
+        return $this->belongsTo(Message::class);
     }
 }
--- database/migrations/2023_12_30_124631_create_messages_table.php
+++ database/migrations/2023_12_30_124631_create_messages_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Message;
 use App\Models\MessageChannel;
 use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
@@ -15,8 +16,16 @@ return new class extends Migration
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
-            /* To which message channel does this message go? */
-            $table->foreignIdFor(MessageChannel::class)->constrained()->cascadeOnDelete();
+            /* To which message channel does this message go?
+               Can be null, if it is a reaction to another message (thread)
+            */
+            $table->foreignIdFor(MessageChannel::class)
+                ->nullable()->constrained()->cascadeOnDelete();
+
+            /* Does this question answer to another one? */
+            $table->foreignIdFor(Message::class)
+                ->nullable()->constrained()->cascadeOnDelete();
+
             /* Who sent this message */
             $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
             $table->string('content');
