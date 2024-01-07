5a98c77a6ce8ecbd10d7ac1f779793faeb434652
Marco Peter <marco@peter-net.ch>
2024-01-07 16:51:44 +0100

    Clean up service worker

--- public/sw.js
+++ public/sw.js
@@ -1,19 +1,16 @@
-function receivePushNotification(event) {
+self.addEventListener("push", (event) => {
     console.log("[Service Worker] Push Received.");
 
-    const data = event.data?.json() ?? { str: "Nix Drinn!"};
+    const data = event.data?.json() ?? { str: "Nix Drinn!" };
     console.log(data);
 
     //call the method showNotification to show the notification
     event.waitUntil(self.registration.showNotification(data.title, data));
-}
+});
 
-function clickPushNotification(event) {
+self.addEventListener("notificationclick", (event) => {
     console.log("[Service Worker] Notification click Received.", event.notification.data);
 
     event.notification.close();
     event.waitUntil(clients.openWindow(event.notification.data["url"]));
-}
-
-self.addEventListener("notificationclick", clickPushNotification);
-self.addEventListener("push", receivePushNotification);
+});
