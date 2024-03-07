9147328f56d0b0ca7667207a31e806a5ad987133
Marco Peter <marco@peter-net.ch>
2024-03-07 22:35:07 +0100

    Add Passport for API login from other applications

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Laravel\Passport\Http\Middleware\CreateFreshApiToken;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group([
+    'middleware' => [
+        'auth:api', CreateFreshApiToken::class
+    ]
+], function () {
+
 });
