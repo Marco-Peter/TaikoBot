a4923273f199f2be65017d7f236e0de4a348b293
Mark Tucker <mark.l.tucker@gmail.com>
2025-08-29 10:45:30 +0200

    Start on styling consistency.

--- resources/js/Pages/Lesson/Edit.vue
+++ resources/js/Pages/Lesson/Edit.vue
@@ -3,6 +3,8 @@ import AppLayout from '@/Layouts/AppLayout.vue';
 import InputLabel from '@/Components/InputLabel.vue';
 import InputError from '@/Components/InputError.vue';
 import TextInput from '@/Components/TextInput.vue';
+import Box from '@/Components/Box.vue';
+import PageContent from '@/Components/PageContent.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import SecondaryButton from '@/Components/SecondaryButton.vue';
 import DangerButton from '@/Components/DangerButton.vue';
@@ -60,140 +62,147 @@ function setNoShow(participant) {
     router.post(route('lessons.setNoShow', props.lesson.id),
         { 'participant': participant.id }, { preserveScroll: true });
 }
+
+function goBack() {
+    window.history.back();
+}
 </script>
 
 <template>
     <AppLayout title="Edit Lesson">
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight flex flex-row items-center gap-1">
                 Edit Lesson
+
+                <div class="flex-1" />
+
+                <Link :href="route('lessons.show', [lesson.id])">
+                    <SecondaryButton small>Open</SecondaryButton>
+                </Link>
+                <Link @click="goBack">
+                    <SecondaryButton small>Back</SecondaryButton>
+                </Link>
             </h2>
         </template>
 
+        <PageContent>
+
         <!-- General Lesson Information -->
-        <div class="pt-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg px-4 py-2">
-                    <form @submit.prevent="submit">
-                        <InputLabel for="title" value="Title" />
-                        <TextInput id="title" v-model="form.title" type="text" class="mt-1 block w-full" />
-                        <InputError :message="form.errors.title" class="mt-2" />
+        <Box>
+            <form @submit.prevent="submit">
+                <InputLabel for="title" value="Title" />
+                <TextInput id="title" v-model="form.title" type="text" class="mt-1 block w-full" />
+                <InputError :message="form.errors.title" class="mt-2" />
 
-                        <div class="mt-4 flex flex-row gap-2">
-                            <div>
-                                <InputLabel for="start" value="Start" />
-                                <TextInput id="start" v-model="form.start" type="datetime-local" class="mt-1 block w-full" />
-                                <InputError :message="form.errors.start" class="mt-2" />
-                            </div>
+                <div class="mt-4 flex flex-row gap-2">
+                    <div>
+                        <InputLabel for="start" value="Start" />
+                        <TextInput id="start" v-model="form.start" type="datetime-local" class="mt-1 block w-full text-xs p-1" />
+                        <InputError :message="form.errors.start" class="mt-2" />
+                    </div>
 
-                            <div>
-                                <InputLabel for="finish" value="Finish" />
-                                <TextInput id="finish" v-model="form.finish" type="datetime-local" class="mt-1 block w-full" />
-                                <InputError :message="form.errors.finish" class="mt-2" />
-                            </div>
-                        </div>
-
-                        <div class="col-span-6 sm:col-span-4 mt-4">
-                            <InputLabel for="notes" value="Notes" />
-                            <textarea id="notes" v-model="form.notes" cols="30" rows="10"
-                                placeholder="Private lesson notes, only visible for teachers"
-                                class="mt-1 block w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"></textarea>
-                            <InputError :message="form.errors.notes" class="mt-2" />
-                        </div>
-
-                        <div class="flex flex-row gap-2 items-stretch mt-4">
-                            <PrimaryButton type="submit">Update Lesson</PrimaryButton>
-                            <Link :href="route('courses.edit', props.lesson.course_id)">
-                                <SecondaryButton>Discard Changes</SecondaryButton>
-                            </Link>
-                        </div>
-                    </form>
+                    <div>
+                        <InputLabel for="finish" value="Finish" />
+                        <TextInput id="finish" v-model="form.finish" type="datetime-local" class="mt-1 block w-full text-xs p-1" />
+                        <InputError :message="form.errors.finish" class="mt-2" />
+                    </div>
                 </div>
-            </div>
-        </div>
+
+                <div class="col-span-6 sm:col-span-4 mt-4">
+                    <InputLabel for="notes" value="Notes" />
+                    <textarea id="notes" v-model="form.notes" cols="30" rows="10"
+                        placeholder="Private lesson notes, only visible for teachers"
+                        class="mt-1 block w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"></textarea>
+                    <InputError :message="form.errors.notes" class="mt-2" />
+                </div>
+
+                <div class="flex flex-row gap-2 items-stretch mt-4">
+                    <PrimaryButton type="submit">Update Lesson</PrimaryButton>
+                    <Link :href="route('courses.edit', props.lesson.course_id)">
+                        <SecondaryButton>Discard Changes</SecondaryButton>
+                    </Link>
+                </div>
+            </form>
+        </Box>
 
         <!-- Participants List -->
-        <div class="pt-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Participants</h2>
-                <div v-for="participant in participants"
-                    class="my-3 px-4 py-2 bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg"
-                    :class="participant.pivot.participation == 'signed_out' ? 'text-gray-500' : ''">
-                    <div class="font-bold text-lg"
-                        :class="participant.pivot.participation == 'signed_out' ? 'line-through' : ''">
-                        <h3>
-                            {{ participant.first_name }}
-                            {{ participant.last_name }}
-                        </h3>
-                        <p v-if="participant.pivot.participation === 'assistance'">Assistant</p>
-                    </div>
-                    <div class="mb-2 italic text-sm">
-                        {{ participant.message }}
-                    </div>
-                    <div v-if="participant.pivot.participation === 'signed_in'" class="flex flex-row gap-2">
-                        <SecondaryButton :small="true" @click="setExcused(participant)">Excused</SecondaryButton>
-                        <SecondaryButton :small="true" @click="setLate(participant)">Late</SecondaryButton>
-                        <SecondaryButton :small="true" @click="setNoShow(participant)">No Show</SecondaryButton>
-                    </div>
-                    <div v-else-if="participant.pivot.participation === 'waitlist'">
-                        On Waitlist
-                        <SecondaryButton class="ml-1" :small="true" @click="setSignedIn(participant)">undo</SecondaryButton>
-                    </div>
-                    <div v-else-if="participant.pivot.participation === 'late'">
-                        Was late
-                        <SecondaryButton class="ml-1" :small="true" @click="setSignedIn(participant)">undo</SecondaryButton>
-                    </div>
-                    <div v-else-if="participant.pivot.participation === 'no_show'">
-                        Did not show up
-                        <SecondaryButton class="ml-1" :small="true" @click="setSignedIn(participant)">undo</SecondaryButton>
-                    </div>
-                    <div v-else-if="participant.pivot.participation === 'signed_out'">
-                        Signed out / excused
-                        <SecondaryButton class="ml-1" :small="true" @click="setSignedIn(participant)">undo</SecondaryButton>
-                    </div>
+        <Box>
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Participants</h2>
+            <div v-for="participant in participants"
+                class=""
+                :class="participant.pivot.participation == 'signed_out' ? 'text-gray-500' : ''">
+                <div class="font-bold text-base"
+                    :class="participant.pivot.participation == 'signed_out' ? 'line-through' : ''">
+                    <h3>
+                        {{ participant.first_name }}
+                        {{ participant.last_name }}
+                    </h3>
+                    <p v-if="participant.pivot.participation === 'assistance'">Assistant</p>
+                </div>
+                <div class="mb-2 italic text-sm">
+                    {{ participant.message }}
+                </div>
+                <div v-if="participant.pivot.participation === 'signed_in'" class="flex flex-row gap-2">
+                    <SecondaryButton :small="true" @click="setExcused(participant)">Excused</SecondaryButton>
+                    <SecondaryButton :small="true" @click="setLate(participant)">Late</SecondaryButton>
+                    <SecondaryButton :small="true" @click="setNoShow(participant)">No Show</SecondaryButton>
+                </div>
+                <div v-else-if="participant.pivot.participation === 'waitlist'">
+                    On Waitlist
+                    <SecondaryButton class="ml-1" :small="true" @click="setSignedIn(participant)">undo</SecondaryButton>
+                </div>
+                <div v-else-if="participant.pivot.participation === 'late'">
+                    Was late
+                    <SecondaryButton class="ml-1" :small="true" @click="setSignedIn(participant)">undo</SecondaryButton>
+                </div>
+                <div v-else-if="participant.pivot.participation === 'no_show'">
+                    Did not show up
+                    <SecondaryButton class="ml-1" :small="true" @click="setSignedIn(participant)">undo</SecondaryButton>
+                </div>
+                <div v-else-if="participant.pivot.participation === 'signed_out'">
+                    Signed out / excused
+                    <SecondaryButton class="ml-1" :small="true" @click="setSignedIn(participant)">undo</SecondaryButton>
                 </div>
             </div>
-        </div>
+        </Box>
 
         <!-- Teachers List -->
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg px-4 py-2">
-                    <h1 class="font-semibold text-xl mb-2">Who's Teaching?</h1>
-                    <table v-if="lessonteachers.length" class="mt-3">
-                        <tbody>
-                            <tr v-for="lessonteacher in lessonteachers" :key="lessonteacher.id">
-                                <td class="pr-5">
-                                    {{ lessonteacher.first_name }}
-                                    {{ lessonteacher.last_name[0] }}.
-                                    {{ lessonteacher.first_name === "Mark" ? "😏" : "" }}
-                                </td>
-                                <td class="pr-5">
-                                    <DangerButton :small="true" @click="removeTeacher(lessonteacher)">Remove</DangerButton>
-                                </td>
-                                <td>{{ lessonteacher.message }}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <p v-else>No Teachers added</p>
+        <Box>
+            <h1 class="font-semibold text-xl mb-2">Who's Teaching?</h1>
+            <table v-if="lessonteachers.length" class="mt-3">
+                <tbody>
+                    <tr v-for="lessonteacher in lessonteachers" :key="lessonteacher.id">
+                        <td class="pr-5">
+                            {{ lessonteacher.first_name }}
+                            {{ lessonteacher.last_name[0] }}.
+                            {{ lessonteacher.first_name === "Mark" ? "😏" : "" }}
+                        </td>
+                        <td class="pr-5">
+                            <DangerButton :small="true" @click="removeTeacher(lessonteacher)">Remove</DangerButton>
+                        </td>
+                        <td>{{ lessonteacher.message }}</td>
+                    </tr>
+                </tbody>
+            </table>
+            <p v-else>No Teachers added</p>
 
-                    <div class="flex flex-row items-center gap-2 mt-4">
-                        <select v-model="newTeacher"
-                                class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800 p-1 text-sm">
-                            <option value="" disabled>--- Select a Teacher ---</option>
-                            <option v-for="teacher in teachers" :key="teacher.id" :value="teacher">{{ teacher.first_name }}
-                            {{
-                            teacher.last_name }}</option>
-                        </select>
-                        <PrimaryButton small :disabled="newTeacher === ''" @click="setTeacher(newTeacher)">
-                            Set
-                        </PrimaryButton>
-                        <PrimaryButton small :disabled="newTeacher === ''" @click="addTeacher(newTeacher)">
-                            Add
-                        </PrimaryButton>
-                    </div>
-                </div>
+            <div class="flex flex-row items-center gap-2 mt-4">
+                <select v-model="newTeacher"
+                        class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800 p-1 text-sm">
+                    <option value="" disabled>--- Select a Teacher ---</option>
+                    <option v-for="teacher in teachers" :key="teacher.id" :value="teacher">{{ teacher.first_name }}
+                    {{
+                    teacher.last_name }}</option>
+                </select>
+                <PrimaryButton small :disabled="newTeacher === ''" @click="setTeacher(newTeacher)">
+                    Set
+                </PrimaryButton>
+                <PrimaryButton small :disabled="newTeacher === ''" @click="addTeacher(newTeacher)">
+                    Add
+                </PrimaryButton>
             </div>
-        </div>
+        </Box>
+
+        </PageContent>
     </AppLayout>
 </template>
