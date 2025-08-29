33697b4fce09d073be80092351271b93e46d6f78
Mark Tucker <mark.l.tucker@gmail.com>
2025-08-29 12:09:19 +0200

    Remove shadow in dashboard.

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -177,7 +177,7 @@ function formatDate(start) {
             <!-- Available courses and workshop to sign up -->
             <div v-if="coursesNotSignedUp.length" class="py-3">
                 <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-                    <div class="overflow-hidden shadow-xl sm:rounded-lg">
+                    <div class="overflow-hidden sm:rounded-lg">
                         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
                             Sign up
                         </h2>
