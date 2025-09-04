8ad35c9572299ea524ce30e51fb49373e8063707
Marco Peter <marco@peter-net.ch>
2025-09-04 15:44:10 +0200

    Update setup script for sail

--- sail_setup.sh
+++ sail_setup.sh
@@ -4,7 +4,7 @@ docker run --rm \
     -u "$(id -u):$(id -g)" \
     -v "$(pwd):/var/www/html" \
     -w /var/www/html \
-    laravelsail/php81-composer:latest \
+    laravelsail/php84-composer:latest \
     composer install --ignore-platform-reqs
 
 cp .env.example .env
