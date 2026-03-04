1b2433e21450db16dcf40e5b8dc0fcb70b1f2ea4
Marco Peter <marco@peter-net.ch>
2026-03-05 00:17:52 +0100

    Reformat user index for mobile devices

--- resources/js/Pages/User/Index.vue
+++ resources/js/Pages/User/Index.vue
@@ -32,59 +32,40 @@ function migrate() {
 
         <PageContent>
 
-        <Box>
-            <table v-if="users.length">
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th class="px-2 text-left">Nickname</th>
-                        <th class="px-2 text-left">First Name</th>
-                        <th class="px-2 text-left">Last Name</th>
-                        <th class="px-2 text-left">Email</th>
-                        <th class="px-2 text-left">Role</th>
-                        <th class="px-2 text-left">Teams</th>
-                        <th class="px-2 text-left">Karma</th>
-                        <th class="px-2 text-left">Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="user in users">
-                        <td class="px-2"><img class="h-8 w-8 rounded-full object-cover"
-                                :src="user.profile_photo_url"
-                                :alt="user.nickname"></td>
-                        <td class="px-2">{{ user.nickname }}</td>
-                        <td class="px-2">{{ user.first_name }}</td>
-                        <td class="px-2">{{ user.last_name }}</td>
-                        <td class="px-2">{{ user.email }}</td>
-                        <td class="px-2">{{ user.role }}</td>
-                        <td class="px-2">{{ user.team ? user.team.name : 'NO GROUP' }}</td>
-                        <td class="px-2">{{ user.karma === null ? '\u{221E}' : user.karma }}</td>
-                        <td>
-                            <div class="flex flex-row items-center gap-2">
-                            <Link :href="route('users.edit', user.id)">
-                                <SecondaryButton small>Edit</SecondaryButton>
-                            </Link>
-                            <DangerButton small @click="destroy(user.id)">Delete</DangerButton>
-                            </div>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-            <p v-else>No Users available</p>
-            <Link :href="route('users.create')">
-                <PrimaryButton class="mt-4">Add User</PrimaryButton>
-            </Link>
-        </Box>
+            <Box>
+                <Link :href="route('users.create')">
+                    <PrimaryButton class="mt-4">Add User</PrimaryButton>
+                </Link>
+            </Box>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <h2 class="font-semibold text-xl mb-2">Danger Zone</h2>
-                <PrimaryButton class="mb-2" @click="migrate()">Run Migrations</PrimaryButton>
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
+            <Box v-if="users.length" v-for="user in users">
+                <div class="flex flex-row items-center">
+                    <div class="w-full">
+                        <h3 class="font-semibold leading-tight">{{ user.nickname }}</h3>
+                        <div>{{ user.first_name }} {{ user.last_name }}</div>
+                        <div>{{ user.email }}</div>
+                    </div>
+                    <div class="w-1/2">
+                        <div>Role: {{ user.role }}</div>
+                        <div>Group: {{ user.team ? user.team.name : 'NO GROUP' }}</div>
+                        <div>Karma: {{ user.karma === null ? '\u{221E}' : user.karma }}</div>
+                    </div>
+                    <div class="w-1/8">
+                        <Link :href="route('users.edit', user.id)">
+                            <SecondaryButton small>Edit</SecondaryButton>
+                        </Link>
+                        <DangerButton small @click="destroy(user.id)">Delete</DangerButton>
+                    </div>
                 </div>
-            </div>
-        </div>
+            </Box>
+            <Box v-else>
+                <p>No Users available</p>
+            </Box>
 
+            <Box>
+                <h2 class="font-semibold text-xl mb-2">Danger Zone</h2>
+                <PrimaryButton class="w-40" @click="migrate()">Run Migrations</PrimaryButton>
+            </Box>
         </PageContent>
-</AppLayout>
+    </AppLayout>
 </template>
